c0c4f95c2230b9ced81826e7eb4b3932deaa535a
pxs <craziesLive@163.com>
2019-07-01 16:24:46 +0800

    测试文件修改

--- src/main/java/com/cn/juli/ServletInitializer.java
+++ src/main/java/com/cn/juli/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MacCountingApplication.class);
+		return application.sources(MacCountingApplication.class);	
 	}
 
 }
